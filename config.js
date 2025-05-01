e6e989d83709faf8afe7d1961348a125ecfdb804
Anneki22 <168829607+Anneki22@users.noreply.github.com>
2025-05-01 17:17:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const conf = {
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "on",
-  AUTO_LIKE: process.env.AUTO_LIKE || "on",
+  AUTO_LIKE: process.env.AUTO_LIKE || "off",
   AUTO_READ_DM: process.env.AUTO_READ_DM || "off",
   AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || "off",
   ADM: process.env.ANTI_DELETE || "on",
-  ON: process.env.OWNER_NAME || "FLASH-MD",
+  ON: process.env.OWNER_NAME || "死_ＮＥＭＥＳ１Ｘ",
   ANTICALL: process.env.ANTICALL || "off",
   ANTIPROMOTE: process.env.ANTIPROMOTE || "off",
   ANTIDEMOTE: process.env.ANTIDEMOTE || "off",
@@ -15,9 +15,9 @@ const conf = {
   PRESENCE_GROUP: process.env.GROUP_PRESENCE || "paused",
   MODE: process.env.MODE || "private",
   AV: process.env.AV || "on",
-  PREFIXES: process.env.PREFIX ? process.env.PREFIX.split(",").map(p => p.trim()).filter(p => p) : [],
+  PREFIXES: process.env.PREFIX ? process.env.PREFIX.split("X").map(p => p.trim()).filter(p => p) : [],
   Session: process.env.SESSION || "",
-  NUMBER: process.env.OWNER_NUMBER || "",
+  NUMBER: process.env.OWNER_NUMBER || "2348077825595",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || ""
 };
